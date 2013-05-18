ecebe99c283a452ff1c2b8ff4d8225c82c749eaa
小林 裕樹 <kyuuki@kobayashi-no-MacBook-Air.local>
2013-05-19 04:43:27 +0900

    Add return page function

---
+++ Kara/Page1ViewController.h
@@ -0,0 +1,13 @@
+//
+//  Page1ViewController.h
+//  Kara
+//
+//  Created by 小林 裕樹 on 2013/05/19.
+//  Copyright (c) 2013年 kyuuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Page1ViewController : UIViewController
+
+@end
---
+++ Kara/Page1ViewController.m
@@ -0,0 +1,42 @@
+//
+//  Page1ViewController.m
+//  Kara
+//
+//  Created by 小林 裕樹 on 2013/05/19.
+//  Copyright (c) 2013年 kyuuki. All rights reserved.
+//
+
+#import "Page1ViewController.h"
+
+@interface Page1ViewController ()
+
+@end
+
+@implementation Page1ViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)page1ReturnSegue:(UIStoryboardSegue *)segue
+{
+}
+
+@end
